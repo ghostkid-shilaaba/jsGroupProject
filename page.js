4834487b9bc6d3e205cbf4ae34e63466013621b0
ghostkid-shilaaba <saadjilal8@gmail.com>
2025-12-15 22:57:03 +0100

    Update page.js

--- page.js
+++ page.js
@@ -19,3 +19,16 @@ homebutton.addEventListener("click",function(){
     homepage.style.display="block";
     mainpage.style.display="none";
 })
+fetch("https://api.themoviedb.org/3/discover/movie?api_key=d8f0b8b451309de8c8b1bb15812e3248")
+    .then(response => response.json())
+    .then(d => {
+        let movies = document.querySelector(".movies");
+        d.results.forEach(movie => {
+            movies.innerHTML+=
+            `<div class="mvelement">
+                <div class="mvpic"><img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"></div>
+                <div class="mvname"><p>${movie.title}</p></div>
+            </div>`
+        });
+    })
+    .catch(error => console.error(error));
